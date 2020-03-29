4cf5a83961f52012fa07adf799ec5775411eec52
rick <github email address>
2020-03-29 13:20:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has Loaded")
+    let l = document.getElementById('text')
+    l.innerHTML= 'This is really cool!'
+})
